ed77252903d90231d094a2baff21b90c5baa13ca
Fully34 <cfullinw34@gmail.com>
2015-06-25 12:24:37 -0600

    finished default page, moving to refresh in place

--- script.js
+++ script.js
@@ -5,25 +5,54 @@ $(document).ready(function() {
     //the form starts as hidden
     var formShow = false;
 
+    if(!formShow) {
+
+        $('.tog-btn').text('Show Form'); // -> Starts Hidden
+    }
+
     $('.tog-btn').on('click', function() {
 
-        if (!formShow){
+        if (!formShow){ // -> If the form is hidden, we are making it un-hidden
             
-            formShow = true;
+            formShow = true; //-> after click, the form will be showing
 
-            $('.form-container').fadeIn(1000);
+            $('.form-container').fadeIn(1); //-> since it was hidden before, we need to fadeIn
 
-        } else if (formShow) {
+            $('.tog-btn').text('Hide Form'); //-> now that the form is showing, we need to change the toggle-button text to 'hide form'
 
-            formShow = false;
+        } else if (formShow) { // -> if the form is showing, we are hiding it
+            
+            formShow = false; //-> after click, the form will be hidden
 
-            $('.form-container').fadeOut();
+            $('.form-container').fadeOut(1); //-> since it was showing before, we need to fadeOut
+
+            $('.tog-btn').text('Show Form'); //-> now that the form is hiding, we need to change the toggle-button text to 'show form'
         }
     });
 
 
 
+//======================== Using Submit Button ==============================//
+    
+    // $('.sub-btn').on('click', function() {
 
+    //     event.preventDefault(); //-> preventing auto-refresh
+
+    //     var newText = $('.input-field').val(); //-> setting variable to whatever the textarea contains
+
+    //     // console.log(newText);
+
+    //     if (newText.length !== 0){ //-> if they don't put anything, we don't want to just delete the contents
+
+    //         $('.bio').text(newText); //-> since there is text in the textarea, we are changing the p.bio to that text 
+
+    //         $('.input-field').val('...'); //-> remove the text from the textarea
+    //     };
+    // });
+
+//============================ Refresh In Place ============================//
+        
+    
 
 
 
